849234d0df99f973fc57303fcb71f4e954aee747
mahintim <mahintim2@gmail.com>
2021-03-21 16:25:17 +0700

    Упрощена поэтапная система компиляции

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-import sys
+import argparse
+import json
 
-from stages import *
+from args import Args
+from ast_to_dict_visitor import AstToDictVisitor
+from errors import Errors
+from parsing.parser import parser
+from semantic.ast_visitor import SemanticVisitor
+from tml_ast import Root
 
-first_stage = ArgumentParsingStage()
 
-first_stage \
-    .set_next(SourceCodeReadingStage()) \
-    .set_next(ParsingStage()) \
-    .set_next(AstVisitingStage()) \
-    .set_next(TypeInferringStage()) \
-    .set_next(OptimizationStage()) \
-    .set_next(CodeGenerationStage())
+def handle_next_stage(result, next_stage=None):
+    if not Errors().is_ok():
+        for error in Errors().list:
+            print(error)
 
-final_result = first_stage.handle(sys.argv)
-exit(0)
+        exit(-1)
+
+    if next_stage is None:
+        return result
+
+    return next_stage(result)
+
+
+def parse_args(_=None):
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('source', help='Путь к файлу с исходным кодом.')
+    arg_parser.add_argument('-p', '--stop-after-parsing',
+                            help='Остановиться после синтаксического анализа и вывести АСД в виде JSON.',
+                            action='store_true')
+
+    args = arg_parser.parse_args()
+    Args().initialize(args)
+
+    return handle_next_stage(None, read_source_code)
+
+
+def read_source_code(_):
+    with open(Args().source, 'r') as file:
+        text = file.read()
+
+    return handle_next_stage(text, parse_source_code)
+
+
+def parse_source_code(text: str):
+    ast = parser.parse(text, tracking=True)
+    if Args().stop_after_parsing and Errors().is_ok():
+        print(json.dumps(AstToDictVisitor().visit(ast)))
+        exit(0)
+
+    return handle_next_stage(ast, visit_ast)
+
+
+def visit_ast(ast: Root):
+    SemanticVisitor().visit_root(ast)
+    return handle_next_stage(None, infer_types)
+
+
+def infer_types(_):
+    return handle_next_stage(None, generate_code)
+
+
+def generate_code(_):
+    return handle_next_stage(None)
+
+
+if __name__ == '__main__':
+    parse_args()
+
